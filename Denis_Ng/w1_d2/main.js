77a17378fa8925764320d818accc3ff4a9e7275d
Denis Ng <denisng89@gmail.com>
2016-06-30 01:00:40 +1000

    w1_d2 homework

--- Denis_Ng/w1_d2/main.js
+++ Denis_Ng/w1_d2/main.js
@@ -1,4 +1,4 @@
-The Age Calculator
+console.log('The Age Calculator')
 
 var currentYear  = 2016;
 var birthYear = 1989;
@@ -6,7 +6,7 @@ var possibleAge  = currentYear - birthYear;
 console.log('They are either ' + possibleAge + ' or ' + (possibleAge - 1));
 
 
-The Lifetime Supply Calculator
+console.log('The Lifetime Supply Calculator')
 
 var currentAge = 27;
 var maxAge = 90;
@@ -14,7 +14,7 @@ var amtPerDay = 5;
 var totalAmtForLife = (maxAge - currentAge) * (365 * amtPerDay);
 console.log('You will need ' + totalAmtForLife + ' bags of Cheetos to last you until the ripe old age of ' + maxAge);
 
-The Geometrizer
+console.log('The Geometrizer')
 
 var rad = 10;
 var circum = rad * 2 * Math.PI;
@@ -22,23 +22,32 @@ console.log('The circumference is ' + circum );
 var area = rad * rad * Math.PI;
 console.log('The area is ' + area);
 
-The even/odd reporter
-
-for(var i=0; i<=20){
+console.log('The Even/Odd Reporter')
+for(var i = 0; i <= 20; i++){
+  console.log(i);        //use this to check during first stage, counter
+}
   if(i % 2===0){
     console.log(i + ' is even');
 }
   else{
     console.log(i + ' is odd');
 }
-//can't figure out error above
 
-Multiplication tables
+// Multiplication tables
 
 for (var i =0; i<=10; i++){
-  var results = i *9;
-    console.log(results)
+  var multiple = i * 9;
+  console.log(i + ' * 9 = ' + multiple);
 }
 
-The Vegan Test
-//Need help completing this last question and Bonus above
+// nested loop means a loop within a loop
+
+// The Vegan Test
+
+var choice = 'steak';
+
+if (choice === ' fruit salad' || choice === 'tofurkey'){
+  console.log('vegan friendly');
+} else {
+  console.log('beware');
+}
