e429d38167363e15988dbce3e6a3df42c0317135
Andrew Pratley <pratley.andrew@gmail.com>
2016-07-06 12:34:08 +1000

    removing unwanted files

--- Andrew_Pratley/w1_d3/objects.js
+++ Andrew_Pratley/w1_d3/objects.js
@@ -11,7 +11,7 @@ console.log(recipe.Title);
 console.log("Serves: " + recipe.Serves);
 console.log("Ingredients: ");
 
-for (i=0; i < recipe.Ingredients.length; i++) {
+for (var i=0; i < recipe.Ingredients.length; i++) {
   console.log(recipe.Ingredients[i]);
 };
 
@@ -35,8 +35,8 @@ var books = [
   },
 ];
 
-for (i=0; i < books.length; i++) {
-  if (books[i].AlreadyRead == true) {
+for (var i=0; i < books.length; i++) {
+  if (books[i].AlreadyRead) { //no need to say == true, because this is a boolean
     console.log("You have already read " + books[i].Title + ' by ' + books[i].Author);
   }
   else {
@@ -49,7 +49,7 @@ for (i=0; i < books.length; i++) {
 var movies = {
   'Title': 'Justice League',
   'Duration': 139,
-  'Stars': ["Hugh Jackman", "Natalie Portman", "Sylvester Stalone"]
+  'Stars': ["Hugh Jackman","Natalie Portman","Sylvester Stalone"]
 };
 
-console.log(movies.Title + " lasts for " + movies.Duration + " minutes. Stars: " + movies.Stars);
+console.log(movies.Title + " lasts for " + movies.Duration + " minutes. Stars: " + movies.Stars.join(', '));// join function converts array to a string and separates them (or not) with whatever you want.
