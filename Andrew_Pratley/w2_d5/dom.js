37159bbdadf6bda4bf95d3a48dbb559de78fe5e9
Andrew Pratley <pratley.andrew@gmail.com>
2016-07-08 14:46:26 +1000

    dom quiz

---
+++ Andrew_Pratley/w2_d5/dom.js
@@ -0,0 +1,31 @@
+
+
+//Q1
+// document.getElementsByTagName('h1')[1];
+//
+// //Q2
+// document.getElementsByTagName('h1')[1].innerHTML = 'Friend';
+
+//Q3
+
+// var button = document.getElementsByTagName('button')[0];
+//
+// function changeRed() {
+//   button.style.backgroundColor = 'red';
+// }
+//
+// button.addEventListener('click', changeRed);
+
+//Q4
+// var favouriteColor = document.querySelector('#favCol').value;
+
+//Q5
+
+// var myArticles = document.querySelector('.my-articles');
+// var node = document.createElement("p");
+// myArticles.appendChild(node);
+
+//Q6
+
+// var list = document.getElementsByTagName("li").length;
+// console.log(list);
