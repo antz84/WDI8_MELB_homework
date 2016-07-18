1e6be4d5fe6acbc92308044f1fd697c79ae97d89
Andrew Pratley <pratley.andrew@gmail.com>
2016-07-18 22:15:37 +1000

    did the bonus

--- Andrew_Pratley/w4_d1/hash_exe.rb
+++ Andrew_Pratley/w4_d1/hash_exe.rb
@@ -27,3 +27,18 @@
 #6 users["Anil"][:favorite_numbers].each { |value|puts value if value.to_f%2==0}
 #7 users["Anil"][:favorite_numbers] & users["Erik"][:favorite_numbers] & users["Jonathan"][:favorite_numbers]
 #8 (users["Anil"][:favorite_numbers] + users["Erik"][:favorite_numbers] + users["Jonathan"][:favorite_numbers]).uniq.sort
+
+# Bonus
+a = ["Andrew", "Sam", "Dean", "Magda", "Tad", "Anthony", "Jordan", "Dennis"]
+groupsOf = 3
+
+people = a
+numLeftOver = people.length % groupsOf
+leftOverArr = people.pop(numLeftOver)
+people = people.each_slice(groupsOf).to_a
+
+i = 0
+while i < leftOverArr.length
+  people[i].push(leftOverArr[i])
+  i += 1
+end
