b895d9b037fdfe2add8d88d8898351d02d1d043e
Anthony Melouney <a.melouney@gmail.com>
2016-07-18 19:21:17 +1000

    homework done

--- Anthony_Melouney/w4/hash_ex2.rb
+++ Anthony_Melouney/w4/hash_ex2.rb
@@ -6,38 +6,39 @@
 a = ["Anil", "Erik", "Jonathan"]
 
 # 1. How would you return the string `"Erik"`?
-  # puts a[1]
+  puts a[1]
 # 2. How would you add your name to the array?
-  # puts a.push('Ant')
+  puts a.push('Ant')
+  # or do it like this:
+  a << "Ant"
 
 # B. Given the following data structure:
 h = {0 => "Zero", 1 => "One", :two => "Two", "two" => 2}
 
 # 1. How would you return the string `"One"`?
-  # puts h[1]
+  puts h[1]
 # 2. How would you return the string `"Two"`?
-  # puts h[:two]
+  puts h[:two]
 # 3. How would you return the number `2`?
-  # puts h["two"]
+  puts h["two"]
 # 4. How would you add `{3 => "Three"}` to the hash?
-  # puts h[3] = "Three"
+  puts h[3] = "Three"
 # 5. How would you add `{:four => 4}` to the hash?
-  # puts h[:four] = 4
+  puts h[:four] = 4
 
 # C. Given the following data structure:
 is = {true => "It's true!", false => "It's false"}
 
 # 1. What is the return value of `is[2 + 2 == 4]`?
-  # puts is[2 + 2 == 4] -->> this returns "It's true!"
+  puts is[2 + 2 == 4] -->> this returns "It's true!"
 # 2. What is the return value of `is["Erik" == "Jonathan"]`?
-  # puts is["Erik" == "Jonathan"] -->> this returns "It's false!"
+  puts is["Erik" == "Jonathan"] -->> this returns "It's false!"
 # 3. What is the return value of `is[9 > 10]`?
-  # puts is[9 > 10] -->> this returns "It's false!"
+  puts is[9 > 10] -->> this returns "It's false!"
 # 4. What is the return value of `is[0]`?
-  # puts is[0]  -->> this returns nothing/nill/nada/zilch
-  # binding.pry
+  puts is[0]  -->> this returns nothing/nill/nada/zilch
 # 5. What is the return value of `is["Erik"]`?
-  # is["Erik"]  -->> this returns nothing/nill/nada/zilch
+  is["Erik"]  -->> this returns nothing/nill/nada/zilch
 
 # D. Given the following data structure:
 users = {
@@ -54,18 +55,36 @@ users = {
     :favorite_numbers => [12, 14, 85],
   },
 }
+
 # 1. How would you access Jonathan's Twitter handle (i.e. the string `"tronathan"`)?
-  # puts users
+  puts users["Jonathan"][:twitter]
 # 2. How would you add the number `7` to Erik's favorite numbers?
-
+  puts users["Erik"][:favorite_numbers].push(7)
+  or this way
+  p users["Erik"][:favorite_numbers] << 7
 # 3. How would you add yourself to the users hash?
-
+  users["Ant"] = {
+    :twitter => "sic",
+    :favorite_numbers => [2, 3, 4],
+    :extra_stuff => "check",
+    }
 # 4. How would you return the array of Erik's favorite numbers?
-
+  p users["Erik"][:favorite_numbers]
 # 5. How would you return the smallest of Erik's favorite numbers?
-
+  p users["Erik"][:favorite_numbers].min
 # 6. How would you return an array of Anil's favorite numbers that are also even?
-
+  puts users["Anil"][:favorite_numbers].select.each_with_index { |i| i.even? }
 # 7. How would you return an array of the favorite numbers common to all users?
-
+  j = [12, 42, 75]
+  e = [8, 12, 24]
+  a = [12, 14, 85]
+  p intersection = (j & e & a)
 # 8. How would you return an array containing all users' favorite numbers, sorted, and excluding duplicates?
+  a = users["Jonathan"][:favorite_numbers]
+  b = users["Erik"][:favorite_numbers]
+  c = users["Anil"][:favorite_numbers]
+  d = (a.concat b.concat c).sort
+  e = d.uniq
+  p e
+
+# binding.pry
