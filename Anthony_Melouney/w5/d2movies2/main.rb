544c98f6e400bcf69c27b0dbf820feaaa5e9e6e4
Anthony Melouney <a.melouney@gmail.com>
2016-07-29 11:31:23 +1000

    finished movies2

--- Anthony_Melouney/w5/d2movies2/main.rb
+++ Anthony_Melouney/w5/d2movies2/main.rb
@@ -1,36 +1,27 @@
-# how to start the virtual server in terminal
-# ruby main.rb
-
-# how this works in terminal...type in terminal
-# pry
-# require 'httparty'
-# HTTParty.get('http..')
-# result = copyaboveline
-# result["Title"]
-
 require 'httparty'
 require 'sinatra'
 require 'sinatra/reloader'
 require 'pry'
 
-get '/' do     # / is the location of the root aka home page
+get '/' do
   erb :index
 end
 
-get '/about' do      # / is the location of the about page
+get '/about' do
   erb :about
 end
 
-get '/result' do       # grabs the result
-  search = params['movie_search'].gsub(/\s+/, "+").strip.downcase
-  @result = HTTParty.get("http://www.omdbapi.com/?s=#{ search }")
-  @search = @result["Search"]
-  erb :result
-end
-# binding.pry
-
 get '/display' do
   title = params['title'].gsub(/\s+/, "+").strip.downcase
-  @display = HTTParty.get("http://www.omdbapi.com/?t=#{ title }")
+  @result = HTTParty.get("http://www.omdbapi.com/?t=#{ title }")
   erb :display
 end
+
+get '/display2' do
+  movies = params['movie_search'].gsub(/\s+/, "+").strip.downcase
+  @display2 = HTTParty.get("http://www.omdbapi.com/?s=#{ movies }")
+  @search = @display2["Search"]
+  erb :display2
+end
+
+# binding.pry
