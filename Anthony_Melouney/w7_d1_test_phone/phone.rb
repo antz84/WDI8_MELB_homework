9d785e0c74e54653b4ab396ffdee14b60cce84ed
Anthony Melouney <a.melouney@gmail.com>
2016-08-08 23:33:40 +1000

    passed all but last two tests

--- Anthony_Melouney/w7_d1_test_phone/phone.rb
+++ Anthony_Melouney/w7_d1_test_phone/phone.rb
@@ -1,13 +1,37 @@
 class Phone
   def initialize(number)
     @number = number.gsub(/\D/, "")
-    # .delete("^0-9")
+  end
+
+  def area_code
+    @number = @number.chars[0..2].to_s
+    # some code to remove new line
+    return @number
+  end
+
+  def to_s
+    @number = @number.chars[0..2]
+    # some code to add brackets
+    # some code to add a space after the brackets
+    # some code to add '-' at -4 from last
+    return @number
   end
 
   def number
-    if @number.length <  10
+    if @number.length > 8 && @number.length < 10
       @number = "0000000000"
       return @number
+    elsif @number.length == 11 && @number.chars.first == '1'
+      @number[0] = ''
+      return @number
+    elsif @number.length == 11 && @number.chars.first != '1'
+      @number = "0000000000"
+      return @number
+    elsif @number.length == 9
+      @number = "0000000000"
+      return @number
+    elsif @number.length == 10 && @number.chars[0..2] == '123'
+      return @number.area_code
     else
       return @number
     end
--- Anthony_Melouney/w7_d1_test_phone/test_phone.rb
+++ Anthony_Melouney/w7_d1_test_phone/test_phone.rb
@@ -33,13 +33,11 @@ class PhoneTest < MiniTest::Test
   end
 
   def test_valid_when_11_digits_and_first_is_1
-    skip
     number = Phone.new("11234567890").number
     assert_equal "1234567890", number
   end
 
   def test_invalid_when_11_digits
-    skip
     number = Phone.new("21234567890").number
     assert_equal "0000000000", number
   end
@@ -50,7 +48,6 @@ class PhoneTest < MiniTest::Test
   end
 
   def test_area_code
-    skip
     number = Phone.new("1234567890")
     assert_equal "123", number.area_code
   end
