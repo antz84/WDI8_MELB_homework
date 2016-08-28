61f750482f9b6979e27efbc4f9e222c0df9099c8
Anthony Melouney <a.melouney@gmail.com>
2016-08-29 09:09:22 +1000

    weekend hw

---
+++ Anthony_Melouney/w9_d5_js_functions/functions.js
@@ -0,0 +1,76 @@
+console.log('JS Functions');
+
+// Question 1
+function maxOfTwoNumbers(a, b) {
+  if(a > b) {
+    return 'Max: ' + a;
+  } else return 'Max: ' + b;
+}
+
+// Question 2
+function maxOfThree(a, b, c) {
+  var max = Math.max(a, b, c);
+  console.log(max);
+}
+
+// Question 3
+function isCharacterAVowel(char) {
+  if(char === 'a' || char === 'e' || char ===  'i' || char === 'o' || char === 'u') {
+    return true;
+  } else return false;
+}
+
+// Question 4
+function sumArray(x) {
+  sum = 0;
+  var arr = [];
+  arr.push(x);
+  for(i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  } return sum;
+}
+
+// Question 4
+function multiplyArray(y) {
+  product = 0;
+  var arr = [];
+  arr.push(y);
+  for(i = 0; i < arr.length; i++) {
+    product *= arr[i];
+  } return sum;
+}
+
+// Question 5
+var numberOfArguments = function(z){
+  return z;
+};
+
+// Question 6
+var reverseString = function (){
+
+};
+
+// Question 7
+function findLongestWord () {
+  arr = [];
+  for(i = 0; i < arr.length; i++) {
+
+  }
+}
+
+// Question 8
+function filterLongWords () {
+  for(i = 0; i < arr.length; i++) {
+    return arr.max;
+  }
+}
+
+
+// Bonus 1
+//??????
+
+
+// Bonus 2
+function charactersOccurencesCount() {
+
+}
